79a15808b1ab108f6c971d3879a323164e25ad47
swkane <samuelwkane@gmail.com>
2018-07-17 16:34:17 -0400

    added images and tweaked styling

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,20 +1,51 @@
-import React from 'react';
-import {Text} from 'react-native';
-import Card from './Card';
-import CardSection from './CardSection';
+import React from "react";
+import { Text, Image, View } from "react-native";
+import Card from "./Card";
+import CardSection from "./CardSection";
 
-const AlbumDetail = props => {
-    return (
-        <Card>
-            <CardSection>
-                <Text>{props.title}</Text>
-            </CardSection>
-            <CardSection>
-                <Text>{props.artist}</Text>
-            </CardSection>
-        </Card>
-    );
+const AlbumDetail = ({ thumbnail_image, title, artist, image }) => {
+  const { thumbnailStyle, headerContentStyle, thumbnailHeaderStyle, headerTextStyle, imageStyle } = styles;
+  return (
+    <Card>
+      <CardSection>
+        <View style={thumbnailHeaderStyle}>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{artist}</Text>
+          <Text>{title}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+          <Image style={imageStyle} source={{ uri: image }} />
+        </CardSection>
+    </Card>
+  );
+};
 
-}
+const styles = {
+  headerContentStyle: {
+    flexDirection: "column",
+    justifyContent: "space-around"
+  },
+  headerTextStyle: {
+      fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumdnailContainerStyle: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+      height: 300,
+      flex: 1,
+      width: null
+  }
+};
 
 export default AlbumDetail;
